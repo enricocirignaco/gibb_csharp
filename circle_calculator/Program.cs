e309633970f72b2bbc344d6009b45ca6043e5ddf
Enrico Cirignaco <e.cirignaco@gmail.com>
2021-01-27 13:05:44 +0100

    new project created

---
+++ circle_calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace circle_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
