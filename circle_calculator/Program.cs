238098a1b0c97dc855359aba4549601f316f4732
Enrico Cirignaco <e.cirignaco@gmail.com>
2021-01-27 14:20:36 +0100

    methods for formulas created

--- circle_calculator/Program.cs
+++ circle_calculator/Program.cs
@@ -6,6 +6,9 @@ namespace circle_calculator
     {
         static void Main(string[] args)
         {
+            // create new calculator object
+            calculator my_calculator = new calculator();
+
             Console.WriteLine("Kreisberechnungen\n_________________\n");
             Console.WriteLine("Wählen Sie eine der folgende Aktionen:\n");
             Console.WriteLine("[1] Kreisumfang berechnen");
@@ -18,6 +21,9 @@ namespace circle_calculator
                 switch(Console.ReadLine())
                 {
                     case "1":
+                        Console.Clear();
+                        Console.WriteLine(my_calculator.Circumference(2));
+                        System.Environment.Exit(1);
                         break;
                     case "2":
                         break;
@@ -37,45 +43,48 @@ namespace circle_calculator
 
         }
     }
-    /*
-    class calculator
+
+    public class calculator
     {
         /*
         * method to calculate circumference.
-        /
-        static UInt16 Circumference(UInt16 radius)
+        */
+        public double Circumference(UInt16 radius)
         {
-            UInt16 circumference = 2 * radius * Math.PI;
+            //formula
+            double circumference = Math.Round((2 * radius * Math.PI), 2);
             return circumference;
         }
 
         /*
         * method to calculate circle area.
-        /
-        static UInt16 Circle_area(UInt16 radius)
+        */
+        public double Circle_area(UInt16 radius)
         {
-            UInt16 circle_area = Math.Pow(radius, 2) * Math.PI;
+            //formula
+            double circle_area = Math.Round((Math.Pow(radius, 2) * Math.PI), 2);
             return circle_area;
         }
 
         /*
         * method to calculate circular arc
-        /
-        static UInt16 Circle_arc(UInt16 radius, UInt16 angle)
+        */
+        public double Circle_arc(UInt16 radius, UInt16 angle)
         {
-            UInt16 circle_arc = (radius * Math.PI * angle)/180;
+            //formula
+            double circle_arc = Math.Round(((radius * Math.PI * angle)/180), 2);
             return circle_arc;
         }
 
         /*
         * method to calculate circular segment
-        /
-        static UInt16 Circle_segment(UInt16 radius, UInt16 angle)
+        */
+        public double Circle_segment(UInt16 radius, UInt16 angle)
         {
-            UInt16 circle_segment = (Math.Pow(radius, 2) * Math.PI * angle)/360;
+            //formula
+            double circle_segment = Math.Round(((Math.Pow(radius, 2) * Math.PI * angle)/360), 2);
             return circle_segment;
-        }
-        
+        } 
     }
-    */
+    
 }
