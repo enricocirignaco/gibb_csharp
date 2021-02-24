832c22e0e3366244668d09b85486a7ddedb5030f
Enrico Cirignaco <e.cirignaco@gmail.com>
2021-02-24 13:07:18 +0100

    new project created

---
+++ auto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
