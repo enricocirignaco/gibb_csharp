18c459b75ecf4d275aeb867d7962bae5d8c7dc0f
Enrico Cirignaco <e.cirignaco@gmail.com>
2021-02-24 14:30:38 +0100

    project coded and tested

--- auto/Program.cs
+++ auto/Program.cs
@@ -6,7 +6,15 @@ namespace auto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            car audi_a6 = new car("Audi", "A6", "BE49025", 60, 5.6);
+            car fiat_panda = new car("Fiat", "Panda", "BE 256854", 30, 6.7);
+            audi_a6.TankUp(60);
+            fiat_panda.TankUp(30);
+            audi_a6.Drive(120);
+            fiat_panda.Drive(80);
+
+            Console.WriteLine(audi_a6.WrongToString());
+            Console.WriteLine(fiat_panda.WrongToString());
         }
     }
 }
